103946d5b51d3a77cae471e0269a71d42aece0cf
Sarajit-mondal <bdsarajit499@gmail.com>
2024-02-02 19:55:24 +0600

    ripple button complete

---
+++ Button Ripple effect/index.js
@@ -0,0 +1,9 @@
+const btnEl = document.getElementById("btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+  const y = event.pageY - btnEl.offsetTop;
+  const x = event.pageX - btnEl.offsetLeft;
+
+  btnEl.style.setProperty("--yPos", y + "px");
+  btnEl.style.setProperty("--xPos", x + "px");
+});
